1d165304e988b41bc7b71cce71e39b91ab7166f5
MR.Fan <yfsoftcom@gmail.com>
2022-08-18 22:04:02 +0800

    add log

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	_ "github.com/team4yf/fpm-go-plugin-ws/plugin"
 	"github.com/team4yf/yf-fpm-server-go/fpm"
 )
@@ -9,5 +11,13 @@ func main() {
 
 	fpmApp := fpm.New()
 	fpmApp.Init()
+	handler := func(topic string, data interface{}) {
+		fmt.Printf("%s %v\n", topic, data)
+	}
+	fpmApp.Subscribe("#ws/connect", handler)
+	fpmApp.Subscribe("#ws/close", handler)
+	fpmApp.Subscribe("#ws/error", handler)
+	fpmApp.Subscribe("#ws/receive", handler)
+
 	fpmApp.Run()
 }
